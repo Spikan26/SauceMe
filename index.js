ad02a3b9911fdc19ffb25bbb4227ef6d0e8d3848
Spikan26 <durieuxalexandre26@gmail.com>
2019-11-26 11:01:25 +0100

    React when it's a file or a link
    Take attachment first, if there's not, take link

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const Sagiri = require("sagiri");
 
-const handler = new Sagiri(process.env.SAGIRI_TOKEN, { 
+const handler = new Sagiri(process.env.SAGIRI_TOKEN, {
 numRes: 1});
 
 
@@ -17,29 +17,54 @@ bot.on("ready", function(){
 
 bot.on("message", async message => {
 	if (message.author.equals(bot.user)) return;
-	
+
 	if (!message.content.startsWith(PREFIX)) return;
-	
+
 	var args = message.content.substring(PREFIX.length).split(" ");
-	
-	var Attachment = (message.attachments).array();
-	message.channel.send("Searching...");
-	let research = handler.getSauce(Attachment[0].url);
-	research.then(function (value) {
-		console.log(value[0]);
-		message.channel.send({"embed": {
-			"title": "[link]",
-			"description": "**SITE :** `" + value[0].site + "`",
-			"url": value[0].url,
-			"color": 3376306,
-			"image": {
-				"url": value[0].thumbnail
-			},
-			"footer": {
-				"text": "similarity : " + value[0].similarity + "%"
-			}
-		}})				
-	})			
+
+		//on a un fichier attaché
+		if((message.attachments).array() != null){
+					var Attachment = (message.attachments).array();
+					message.channel.send("Searching...");
+					let research = handler.getSauce(Attachment[0].url);
+					research.then(function (value) {
+						console.log(value[0]);
+						message.channel.send({"embed": {
+							"title": "[link]",
+							"description": "**SITE :** `" + value[0].site + "`",
+							"url": value[0].url,
+							"color": 3376306,
+							"image": {
+								"url": value[0].thumbnail
+							},
+							"footer": {
+								"text": "similarity : " + value[0].similarity + "%"
+							}
+						}})
+					})
+		//aucun fichier fourni, on test s'il y a un lien
+	} else if(args[1] != null){
+
+		message.channel.send("Searching...");
+		let research = handler.getSauce(args[1]);
+		research.then(function (value) {
+			console.log(value[0]);
+			message.channel.send({"embed": {
+				"title": "[link]",
+				"description": "**SITE :** `" + value[0].site + "`",
+				"url": value[0].url,
+				"color": 3376306,
+				"image": {
+					"url": value[0].thumbnail
+				},
+				"footer": {
+					"text": "similarity : " + value[0].similarity + "%"
+				}
+			}})
+		})
+	} else {
+		message.channel.send("No file provided")
+	}
 })
 
 bot.login(process.env.BOT_TOKEN)
